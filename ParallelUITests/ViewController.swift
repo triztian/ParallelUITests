28c11909342b2cd410a0bb561111b936c4acf50f
Tristian Azuara <tristian@airspacetechnologies.com>
2020-09-12 16:07:18 -0700

    Initial Commit

---
+++ ParallelUITests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParallelUITests
+//
+//  Created by Tristian Azuara on 9/12/20.
+//  Copyright © 2020 Tristian Azuara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
